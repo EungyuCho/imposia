7234860123f34e67e036f859db2ad94a4d32673c
Asa <EungyuCho@users.noreply.github.com>
2026-07-20 00:50:20 +0900

    feat(demo): align Imposia branding and viewer styles

---
+++ packages/viewer/src/brand-mark.ts
@@ -0,0 +1,4 @@
+export const IMPOSIA_BRAND_MARK = `<svg class="imposia-mark" viewBox="0 0 72 72" aria-hidden="true" focusable="false">
+  <path class="imposia-mark-outline" d="M36 20 18 9 4 17l18 11v17L4 56l16 9 16-10V20Zm0 0L54 9l14 8-18 11v17l18 11-16 9-16-10V20Z" />
+  <path class="imposia-mark-fold" d="m22 28 14-8v16L22 28Zm14 27V39l14 9-14 7Z" />
+</svg>`;
